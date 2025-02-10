e10f0789272d1e8fef9b3c494b5f425df8af5a40
Inkarzero <168024485+Inkarzero@users.noreply.github.com>
2025-02-10 21:55:54 +0100

    Updated data source code

--- op-node/rollup/derive/data_source.go
+++ op-node/rollup/derive/data_source.go
@@ -65,22 +65,28 @@ func NewDataSourceFactory(log log.Logger, cfg *rollup.Config, fetcher L1Fetcher,
 
 // OpenData returns the appropriate data source for the L1 block `ref`.
 func (ds *DataSourceFactory) OpenData(ctx context.Context, ref eth.L1BlockRef, batcherAddr common.Address) (DataIter, error) {
-	ds.log.Debug("optimism/op-node/rollup/derive/data_source.go | OpenData | ", "ref", ref, "batcherAddr", batcherAddr)
+	ds.log.Debug("optimism/op-node/rollup/derive/data_source.go | OpenData | started", "ref", ref, "batcherAddr", batcherAddr)
 	// Creates a data iterator from blob or calldata source so we can forward it to the altDA source
 	// if enabled as it still requires an L1 data source for fetching input commmitments.
 	var src DataIter
 	if ds.ecotoneTime != nil && ref.Time >= *ds.ecotoneTime {
+		ds.log.Debug("optimism/op-node/rollup/derive/data_source.go | OpenData | ecotone upgrade active", "ref", ref, "batcherAddr", batcherAddr)
 		if ds.blobsFetcher == nil {
+			ds.log.Debug("optimism/op-node/rollup/derive/data_source.go | OpenData | stopped, ecotone upgrade active but blobs fetcher not configured", "ref", ref, "batcherAddr", batcherAddr)
 			return nil, fmt.Errorf("ecotone upgrade active but beacon endpoint not configured")
 		}
+		ds.log.Debug("optimism/op-node/rollup/derive/data_source.go | OpenData | ecotone upgrade active, using blob data source", "ref", ref, "batcherAddr", batcherAddr)
 		src = NewBlobDataSource(ctx, ds.log, ds.dsCfg, ds.fetcher, ds.blobsFetcher, ref, batcherAddr)
 	} else {
+		ds.log.Debug("optimism/op-node/rollup/derive/data_source.go | OpenData | using calldata source", "ref", ref, "batcherAddr", batcherAddr)
 		src = NewCalldataSource(ctx, ds.log, ds.dsCfg, ds.fetcher, ref, batcherAddr)
 	}
 	if ds.dsCfg.altDAEnabled {
 		// altDA([calldata | blobdata](l1Ref)) -> data
+		ds.log.Debug("optimism/op-node/rollup/derive/data_source.go | OpenData | altDA enabled", "ref", ref, "batcherAddr", batcherAddr)
 		return NewAltDADataSource(ds.log, src, ds.fetcher, ds.altDAFetcher, ref), nil
 	}
+	ds.log.Debug("optimism/op-node/rollup/derive/data_source.go | OpenData | no data source", "ref", ref, "batcherAddr", batcherAddr)
 	return src, nil
 }
 
