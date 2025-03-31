6b0f27b6a39663ff38858cd6b701d5383bb9bb4d
Inkarzero <168024485+Inkarzero@users.noreply.github.com>
2025-03-31 15:53:31 +0200

    Removed L1blocks from log

--- op-node/rollup/derive/batches.go
+++ op-node/rollup/derive/batches.go
@@ -34,7 +34,7 @@ const (
 func CheckBatch(ctx context.Context, cfg *rollup.Config, log log.Logger, l1Blocks []eth.L1BlockRef,
 	l2SafeHead eth.L2BlockRef, batch *BatchWithL1InclusionBlock, l2Fetcher SafeBlockFetcher,
 ) BatchValidity {
-	log.Debug("/op-node/rollup/derive/batches.go | CheckBatch | started", "batch", batch, "batch_type", batch.GetBatchType(), "l1_blocks", l1Blocks, "l2_safe_head", l2SafeHead)
+	log.Debug("/op-node/rollup/derive/batches.go | CheckBatch | started", "batch", batch, "batch_type", batch.GetBatchType(), "len(l1_blocks)", len(l1Blocks), "l2_safe_head", l2SafeHead)
 	switch typ := batch.GetBatchType(); typ {
 	case SingularBatchType:
 		singularBatch, ok := batch.AsSingularBatch()
